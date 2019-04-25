bdb27ff7c4e1b86c111776498003c49e6f16bd51
jesusquev3311 <jesusquev3311@gmail.com>
2019-04-25 06:55:09 -0500

    styles added and worked on oage template

--- wp-content/themes/learningWordpress/special-template.php
+++ wp-content/themes/learningWordpress/special-template.php
@@ -10,6 +10,10 @@ if(have_posts()){
 ?>
     <article class="post page">
         <h2><<?php the_title() ?></h2>
+        <div class="info-box">
+            <h4>disclaimer title</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed libero rerum, consequuntur dolorum maxime esse eligendi at eos totam, corporis mollitia quas aut doloremque minima aspernatur iure est necessitatibus nobis?</p>
+        </div>
         <p><?php the_content() ?></p>
     </article>
 <?php
